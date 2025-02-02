04fef00789ff74682d4908cac9fe83219d09beed
parthusandeep <sandeepparthu@gmail.com>
2025-02-02 14:39:30 +0530

    Add files via upload

---
+++ fresh.js
@@ -0,0 +1,16 @@
+let targetdate = new Date("jan 26, 2025,00:00:00").getTime();
+
+let x =setInterval(function () {
+    let now = new Date().getTime();
+    let distance =  now- targetdate;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+}, 1000);
